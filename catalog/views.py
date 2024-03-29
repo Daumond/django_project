65fa1d6322890ebad02a63caa84f8e2c5165a8c2
Dmitry <kde98@mail.ru>
2024-03-29 23:33:27 +0300

    added message

--- catalog/views.py
+++ catalog/views.py
@@ -11,5 +11,5 @@ def contacts(request):
         name = request.POST.get('name')
         phone = request.POST.get('phone')
         message = request.POST.get('message')
-        print(f'You have new message from {name} {phone}')
+        print(f'You have new message from {name} {phone}: {message}')
     return render(request, 'catalog/contacts.html')
