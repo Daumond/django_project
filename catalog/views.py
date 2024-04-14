6f029d10d9fb6266f5f7528e9185fa1c3e66677b
Dmitry <kde98@mail.ru>
2024-04-14 21:38:01 +0300

    deleted legacy

--- catalog/views.py
+++ catalog/views.py
@@ -3,30 +3,11 @@ from django.views.generic import ListView, DetailView, TemplateView
 from catalog.models import Product
 
 
-# Create your views here.
-
-# def home(request):
-#     product_list = Product.objects.all()
-#     context = {
-#         'object_list': product_list,
-#         'title': 'Список товаров'
-#     }
-#     return render(request, 'catalog/home.html',  context)
 class ProductListView(ListView):
     model = Product
     template_name = 'catalog/home.html'
 
 
-# def contacts(request):
-#     if request.method == 'POST':
-#         name = request.POST.get('name')
-#         phone = request.POST.get('phone')
-#         message = request.POST.get('message')
-#         print(f'You have new message from {name} {phone}: {message}')
-#     context = {
-#         'title': 'Контакты'
-#     }
-#     return render(request, 'catalog/contacts.html', context)
 class ContactsView(TemplateView):
     template_name = 'catalog/contacts.html'
 
@@ -44,19 +25,6 @@ class ContactsView(TemplateView):
         return context
 
 
-# def product(request):
-#     product_list = Product.objects.all()
-#     context = {
-#         'object_list': product_list,
-#         'title': 'Продукты'
-#     }
-#     return render(request, 'catalog/product.html', context)
 class ProductDetailView(DetailView):
     model = Product
     template_name = 'catalog/product_item.html'
-
-# def product_item(request, pk):
-#     context = {
-#         'object': Product.objects.get(pk=pk)
-#     }
-#     return render(request, 'catalog/product_item.html', context)
