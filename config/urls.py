d2ebb5e90bb9e3c2ac35123c6031f4f71e14f667
Dmitry <kde98@mail.ru>
2024-04-28 01:47:07 +0300

    images fixes

--- config/urls.py
+++ config/urls.py
@@ -17,10 +17,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from catalog.views import ContactsView
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('contacts/', ContactsView.as_view(), name='contacts'),
     path('', include('catalog.urls', namespace='catalog')),
     path('blog/', include('blog.urls', namespace='blog')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
